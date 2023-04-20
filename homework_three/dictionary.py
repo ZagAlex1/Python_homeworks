732b9d873a37e4b9b172c45bda9686fab03335db
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-04-21 01:41:52 +0300

    finished dictionary

--- homework_three/dictionary.py
+++ homework_three/dictionary.py
@@ -4,12 +4,19 @@
 # Q, Z – 10 очков. А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко;
 # Д, К, Л, М, П, У – 2 очка; Б, Г, Ё, Ь, Я – 3 очка; Й, Ы – 4 очка; Ж, З, Х, Ц, Ч – 5 очков;
 # Ш, Э, Ю – 8 очков; Ф, Щ, Ъ – 10 очков. Напишите программу, которая вычисляет стоимость
-# введенного пользователем слова. Будем считать, что на вход подается только одно слово, ёкоторое
+# введенного пользователем слова. Будем считать, что на вход подается только одно слово, которое
 # содержит либо только английские, либо только русские буквы.
 # *Пример:*
 #
 # ноутбук
 #     12
+
+my_word = input("Введите слово для, вычисления его стоимости: ")
+my_word_one = my_word.upper()
+print(my_word)
+
+amount_of_cost = 0
+
 
 my_list_russian_one = ['А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']  # 1 очко
 my_list_russian_two = ['Д', 'К', 'Л', 'М', 'П', 'У']  # 2 очка
@@ -32,3 +39,25 @@ my_dict_english_list = [{1: my_english_one}, {2: my_english_two}, {3: my_english
                         {5: my_english_five},
                         {8: my_english_eight}, {10: my_english_ten}]
 
+for item in my_dict_russian_list:
+    for value in item.values():
+        for letter in value:
+            if letter in my_word_one:
+                amount_of_cost += sum(item.keys())
+
+for item in my_dict_english_list:
+    for value in item.values():
+        for letter in value:
+            if letter in my_word_one:
+                amount_of_cost += sum(item.keys())
+
+print(amount_of_cost)
+
+# def count_letters(dict, word, cost):
+#     for item in dict:
+#         for value in item.values():
+#             for letter in value:
+#                 if letter in word:
+#                     cost += sum(item.keys())
+#     return cost
+# print(count_letters(my_dict_russian_list, my_word_one, amount_of_cost))
